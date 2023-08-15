8ac3b0171512163992da0ae59477695ab1551f63
Jefferson Santos <jheffersonn@hotmail.com>
2023-08-15 10:05:36 -0300

    tela criar

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Context;
+using System.Linq;
 
 namespace ProjetoMVC.Controllers
 {
     public class ContatoController : Controller
     {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context) 
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var contatos = _context.Contatos.ToList();
+            return View(contatos);
+        }
+
+        public IActionResult Criar()
+        {
             return View();
+        
         }
     }
 }
